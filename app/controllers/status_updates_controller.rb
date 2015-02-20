3db6c03107983bf55310c2c3a3d551adf523a1bb
Finnegan Hewitt <fbhewitt@gmail.com>
2015-02-20 11:28:13 -0700

    Add status update controller, user can view status update index page

---
+++ app/controllers/status_updates_controller.rb
@@ -0,0 +1,47 @@
+class StatusUpdatesController < ApplicationController
+  before_action :set_status_update, only: [:show, :edit, :update, :destroy]
+  def index
+    @status_updates = StatusUpdate.all
+  end
+
+  def show
+  end
+
+  def new
+  @status_update = StatusUpdate.new
+  end
+
+  def create
+    if @status_update.save
+      redirect_to status_update_path(@status_update), notice: "Status update was successfully created"
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @status_update.update
+      redirect_to status_update_path(@status_update), notice: "Status update was successfully updated"
+    else render :edit
+    end
+  end
+
+  def destroy
+    @status_update.destroy
+    if @status_update.destroy
+      redirect_to status_updates, notice: "Status update was successfully destroyed"
+    end
+  end
+
+private
+    def status_update_params
+      params.require(:status_update).permit(:status, :user, :number_of_likes)
+    end
+
+    def set_status_update
+      @status_update = StatusUpdate.find(params[:id])
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150220173938) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "status_updates", force: :cascade do |t|
+    t.text     "status"
+    t.string   "user"
+    t.integer  "number_of_likes"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
 
 end
